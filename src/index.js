58f776e52d8b1162967a021fe2695e9a5df7bbf0
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-06-14 18:25:17 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 document.addEventListener('DOMContentLoaded', run);
 function run() {
   const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  const letter = document.getElementById("breed-dropdown");
   fetch(imgUrl)
   .then(function(response){
     return response.json();
@@ -22,9 +23,20 @@ fetch(breedUrl)
   console.log(json);
   console.log(json.message.length);
   let breeds = document.getElementById("dog-breeds");
-  for (let i=0;i<20;i++){
-    breeds.insertAdjacentHTML('beforeend',`<li>${json.message[i]}</li>`)
+  for (const key in json.message){
+    breeds.insertAdjacentHTML('beforeend',`<li>${key}</li>`);
+  }
+  letter.addEventListener('changed', filter);
+  function filter(){
+    for (const key in json.message){
+      if(key[0] === letter.value){
+      breeds.insertAdjacentHTML('beforeend',`<li>${key}</li>`);
+    }
+  }
+
   }
 
 });
+
+
 }
